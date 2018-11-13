63d2b980024dbea67e81f76db3018797e49a21ea
Amit Kumar Singh <amit.aanmol@gmail.com>
2018-11-13 23:57:57 +0530

    Added showAlert function to show alert popup

--- AAUtil/AAUtil.swift
+++ AAUtil/AAUtil.swift
@@ -6,6 +6,19 @@
 //  Copyright © 2018 Amit Kumar Singh. All rights reserved.
 //
 
+import UIKit
+
 class AAUtil {
+    
+    static func showAlert(title: String, message: String, controller: UIViewController) -> Void {
+        // create the alert
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        // show the alert top of all windows
+        DispatchQueue.main.async(execute: {
+            controller.present(alert, animated: true, completion: nil)
+        })
+    }
 
 }
